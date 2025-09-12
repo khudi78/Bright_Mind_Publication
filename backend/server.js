252cff51d99fdc0cea872914c160e89a4be2b8d1
Khushi Kumari <krkhushi2001@gmail.com>
2025-09-12 09:46:15 +0530

    3rd commit

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,7 @@ app.post("/send-email", async (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
