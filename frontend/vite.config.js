4151e207f43ed351f4aef44d696a5a3f739a623c
Khushi Kumari <krkhushi2001@gmail.com>
2025-09-12 10:54:27 +0530

    7th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   css: {
-    transformer: 'postcss', // force PostCSS instead of lightningcss
+    transformer: 'postcss', // 👈 force PostCSS instead of lightningcss
   },
 })
